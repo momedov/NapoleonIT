a08f495461fd0f88d0bef60dfdbedd306cf0b293
momedov <74566023+momedov@users.noreply.github.com>
2020-11-17 12:16:14 +0500

    Update r2i.py

--- r2i.py
+++ r2i.py
@@ -2,9 +2,9 @@
 class Solution:
 	def romanToInt(self, s: str) -> int:
 
-		sym=['M','D','C','L','X','V','I']
+		sym = ['M','D','C','L','X','V','I']
 
-		d={'M':1000, 'D': 500, 'C': 100, 'L':50, 'X':10, 'V':5, 'I':1}
+		d = {'M':1000, 'D': 500, 'C': 100, 'L':50, 'X':10, 'V':5, 'I':1}
 
 		sum = 0 
 		i = 0 
